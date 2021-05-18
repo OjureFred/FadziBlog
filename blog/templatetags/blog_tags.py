fa1fcd93d22c4e8cd1707b8aaed31b0b1d9be78f
Fredrick Ojure <fredojure@hotmail.com>
2021-05-18 16:11:38 +0300

    Added code for most commented posts to base.html

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.simple_tag
 def get_most_commented_posts(count=5):
-    return Post.published.annotate(total_comments=Count('comments')).order_by('-total-comments')[:count]
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
 
 @register.simple_tag
 def total_posts():
