91c800c9dcf4fd1892988bd6dd864d7dd5c5e8a1
Fredrick Ojure <fredojure@hotmail.com>
2021-05-18 09:48:46 +0300

    Completed code for pagination and display of blogs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.core import send_mail
+from django.core.mail import send_mail
 
 from .models import Post
 from .forms import EmailPostForm
