eddf7ec4123bb6a8a50a6ef8756a27acdbda8289
Fredrick Ojure <fredojure@hotmail.com>
2021-05-18 16:35:13 +0300

    Added markdown filters to list and detail displays

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,8 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+
+import markdown
 
 from ..models import Post
 
@@ -17,3 +20,7 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
